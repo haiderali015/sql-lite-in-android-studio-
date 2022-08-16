738c639327552ff914a51d27a6cdc29ac95dd638
haider ali <ha347278@gmail.com>
2022-08-16 22:15:38 +0500

    cmt

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.todoapp;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
