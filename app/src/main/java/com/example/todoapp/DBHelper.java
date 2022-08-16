04f082beced4ca0549e2d913272135ea6aa612c0
haider ali <ha347278@gmail.com>
2022-08-16 22:59:41 +0500

    db helper class 2

--- app/src/main/java/com/example/todoapp/DBHelper.java
+++ app/src/main/java/com/example/todoapp/DBHelper.java
@@ -10,5 +10,13 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class DBHelper {
-}
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String STUDENT_ID = "StudentID";
+    public static final String STUDENT_NAME = "StudentName";
+    public static final String STUDENT_ROLL = "StudentRollNumber";
+    public static final String STUDENT_ENROLL = "IsEnrolled";
+    public static final String STUDENT_TABLE = "StudentTable";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, "MyDB.db", null, 1);
+    }
