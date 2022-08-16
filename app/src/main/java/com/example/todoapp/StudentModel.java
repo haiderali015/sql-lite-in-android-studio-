fdd22e27e976c58942ccb42b6bba9fd572e284ed
haider ali <ha347278@gmail.com>
2022-08-16 23:15:05 +0500

    student mdel complete

--- app/src/main/java/com/example/todoapp/StudentModel.java
+++ app/src/main/java/com/example/todoapp/StudentModel.java
@@ -13,4 +13,32 @@ public class StudentModel {
                 ", isEnroll=" + isEnroll +
                 '}';
     }
-    
+    public StudentModel(String name, int rollNmber, boolean isEnroll) {
+        this.name = name;
+        this.rollNmber = rollNmber;
+        this.isEnroll = isEnroll;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNmber() {
+        return rollNmber;
+    }
+
+    public void setRollNmber(int rollNmber) {
+        this.rollNmber = rollNmber;
+    }
+
+    public boolean isEnroll() {
+        return isEnroll;
+    }
+
+    public void setEnroll(boolean enroll) {
+        isEnroll = enroll;
+    }
+}
